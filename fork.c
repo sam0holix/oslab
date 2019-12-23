43c6fe811f76277d7d3143e25f422f6d1918177b
sam0holix <43233525+sam0holix@users.noreply.github.com>
2019-12-23 14:31:39 +0530

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main() {
+  pid_t pid;
+
+  pid = fork();
+
+  if(pid < 0) {
+    fprintf(stderr, "Fork failed\n");
+    return 0;
+  }
+  else if (pid == 0) {
+    execlp("/bin/ls","ls",NULL);
+  }
+  else {
+    wait(NULL);
+    printf("Child Complete\n");
+  }
+  return 0;
+}
